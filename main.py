c3ad8095bdd0519c2fbf2799a143edddaccf0af7
Vektori <78300988+Vektori@users.noreply.github.com>
2026-01-11 22:05:40 +0200

    Added resquest package and llama for chatting feature

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+#%%
+url = "http://localhost:11434/api/chat"
+
+payload = {
+    "model": "qwen2.5-coder:14b",
+    "messages": [
+        {
+            "role": "user",
+            "content": "Write a Python function that calculates the factorial of a number using recursion."
+        }
+    ]
+}
+
+response = requests.post(url, json=payload, stream=True)
+
+if response.status_code == 200:
+
+# %%
